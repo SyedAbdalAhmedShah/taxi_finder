ab5d2224d14d0d5e972e7ffac6e95cbab63cd872
Syed Abdal <syedahmad123123shah@gmail.com>
2024-12-07 01:39:26 +0500

    stuff

--- lib/blocs/user_map_bloc/shuttle_finder_bloc/bloc/shuttle_finder_bloc.dart
+++ lib/blocs/user_map_bloc/shuttle_finder_bloc/bloc/shuttle_finder_bloc.dart
@@ -1,13 +1,10 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'shuttle_finder_event.dart';
 part 'shuttle_finder_state.dart';
 
 class ShuttleFinderBloc extends Bloc<ShuttleFinderEvent, ShuttleFinderState> {
   ShuttleFinderBloc() : super(ShuttleFinderInitial()) {
-    on<ShuttleFinderEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<ShuttleFinderEvent>((event, emit) {});
   }
 }
--- lib/blocs/user_map_bloc/shuttle_finder_bloc/bloc/shuttle_finder_state.dart
+++ lib/blocs/user_map_bloc/shuttle_finder_bloc/bloc/shuttle_finder_state.dart
@@ -1,6 +1,5 @@
 part of 'shuttle_finder_bloc.dart';
 
-@immutable
 sealed class ShuttleFinderState {}
 
 final class ShuttleFinderInitial extends ShuttleFinderState {}
